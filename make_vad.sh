917e4d251edc799ad190ee1091df3111ac0340b7
Sebastian Trueg <trueg@kde.org>
2012-11-08 17:11:07 +0100

    Added VAD sticker and make_vad script to build the vad package.

---
+++ make_vad.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# FIXME: check if vadpacker is installed
+# Path to the vadpacker tool
+VADPACKER=vadpacker.py
+
+VERSION="0.1"
+
+# Additional file list
+FILES=`find . -name "*.css" -or -name "*.js" -or -name "*.png" -or -name "*.gif"`
+
+$VADPACKER -o ods-console-$VERSION.vad -t "ods-console/" --var="VERSION=$VERSION" ods-console-sticker.xml $FILES
